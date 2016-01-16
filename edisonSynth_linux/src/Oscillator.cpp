008bc70087e873929c1f380f2aa6fe6d73d73474
Philipp Fuerholz <fuerholz@gmx.ch>
2016-01-16 14:22:46 +0100

    beginn umbau oszillator auf wavetable

--- edisonSynth_linux/src/Oscillator.cpp
+++ edisonSynth_linux/src/Oscillator.cpp
@@ -22,49 +22,17 @@ void Oscillator::set_waveform(int wf)
 
 Oscillator::Oscillator()
 {
-	sinewaves = new short[SINE_SAMPLES];
-	harm_coeffs1=new double[512];
-	harm_coeffs2=new double[512];
 	samples_to_interpolate=FRAMES_BUFFER;
 	interp_cntr=0;
 	current_phase=0;
 	current_frequency=0;
 	current_symm=1.0;
-	f_cutoff=20000;
-	resonance=0;
 
-	ifstream wt_in;
-	wt_in.open("sine.tab",ios::binary|ios::in);
-	short bfrval;
-	for(int z=0;z<SINE_SAMPLES;z++)
-	{
-		wt_in.read(reinterpret_cast<char*>(&bfrval),sizeof(short));
-		*(sinewaves + z)=bfrval;
-	}
-	wt_in.close();
+
 
 }
 
-void Oscillator::set_fcutoff(double fc)
-{
-	if(fc<20)
-	{
-		f_cutoff=20;
-	}
-	else if(fc > 20000)
-	{
-		f_cutoff=20000;
-	}
-	else
-	{
-		f_cutoff = fc;
-	}
-}
 
-void Oscillator::set_resonance(double k)
-{
-	resonance=k;
-}
 
 void Oscillator::recalc_coeffs(int nsamples,double nfreq)
 {
@@ -72,14 +40,13 @@ void Oscillator::recalc_coeffs(int nsamples,double nfreq)
 	double* harm_killer;
 	double raw_val;
 	double f_harm;
-	double q;
 	double filter_coeff;
 	double sum_coeffs=0;
 	coeffs2=new double[512];
 	for(int ii=0;ii<512;ii++)
 	{
 		f_harm=current_frequency*ii;
-		q=(1.0+(f_harm/f_cutoff)*(f_harm/f_cutoff));
+		//q=(1.0+(f_harm/f_cutoff)*(f_harm/f_cutoff));
 		filter_coeff = 1.0;// 1.0/sqrt(q*q*q*q + resonance*resonance -2*resonance*q*q*cos(4*atan(f_harm/f_cutoff)));
 		if(waveform==0) // square wave
 		{
@@ -124,8 +91,8 @@ void Oscillator::recalc_coeffs(int nsamples,double nfreq)
 	//}
 	//else
 	//{
-		harm_killer=harm_coeffs1;
-		harm_coeffs1=coeffs2;
+		//harm_killer=harm_coeffs1;
+		//harm_coeffs1=coeffs2;
 		frequency_1=nfreq;
 	//	coeffs_active=1;
 	//}
@@ -140,64 +107,35 @@ void Oscillator::set_symm(double s)
 
 double Oscillator::get_nextval()
 {
-	int harm_cntr=1;
 
-	int intphase;
-	double harm_phase;
 	double sample_val=0;
 	double harm_coeff;
 
-	harm_phase=current_phase;
 
-	///if(coeffs_active==1)
-	//{
-		harm_coeff=harm_coeffs1[0];// + (harm_coeffs2[0] - harm_coeffs2[0])*((double)interp_cntr/(double)samples_to_interpolate);
-		current_frequency = frequency_1;// + (frequency_2 - frequency_1)*((double)interp_cntr/(double)samples_to_interpolate);
-	//}
-	//else
-	//{
-	//	harm_coeff=harm_coeffs2[0];// + (harm_coeffs1[0] - harm_coeffs2[0])*((double)interp_cntr/(double)samples_to_interpolate);
-	//	current_frequency = frequency_2;// + (frequency_1 - frequency_2)*((double)interp_cntr/(double)samples_to_interpolate);
-	//}
-
-	d_phase = 1;//(double)SINE_SAMPLES*current_frequency/(double)SAMPLING_RATE;
-	sample_val+=harm_coeff;
-
-	while(current_frequency*harm_cntr<F_LIMIT)
+	if(coeffs_active==1)
 	{
-		//if(coeffs_active==1)
-		//{
-			harm_coeff=harm_coeffs1[harm_cntr];// + (harm_coeffs2[harm_cntr] - harm_coeffs1[harm_cntr])*((double)interp_cntr/(double)samples_to_interpolate);
-		//}
-		//else
-		//{
-		//	harm_coeff=harm_coeffs1[harm_cntr];// + (harm_coeffs2[harm_cntr] - harm_coeffs1[harm_cntr])*((double)interp_cntr/(double)samples_to_interpolate);
-		//}
-		intphase=(int)harm_phase;
-
-		if(waveform==1)
-		{
-			sample_val+=sin(harm_phase*2.0*M_PI/SINE_SAMPLES)*harm_coeff;
-		}
-		else
-		{
-			sample_val+=cos(harm_phase*2.0*M_PI/SINE_SAMPLES)*harm_coeff;
-		}
-
-		//sample_val=sample_val << 2;
-		harm_phase+=current_phase;
-		if (harm_phase > SINE_SAMPLES)
-		{
-			harm_phase -= SINE_SAMPLES;
-		}
-		harm_cntr++;
+		current_symm =  symm1 + (symm2-symm1)*((double)interp_cntr/(double)samples_to_interpolate);
+		current_frequency = frequency_1 - (frequency_2-frequency_1)*((double)interp_cntr/(double)samples_to_interpolate);
 	}
-	//cout << " harmonics used: " << harm_cntr << endl;
+	else
+	{
+		current_symm =  symm2 + (symm1-symm2)*((double)interp_cntr/(double)samples_to_interpolate);
+		current_frequency = frequency_2 - (frequency_1-frequency_2)*((double)interp_cntr/(double)samples_to_interpolate);
+	}
+	d_phase = (double)SINE_SAMPLES*current_frequency/(double)SAMPLING_RATE;
+
+	int symmInt=(int)(current_symm*255);
+
 	current_phase+=d_phase;
+
 	if(current_phase >= SINE_SAMPLES)
 	{
 		current_phase -= (double)SINE_SAMPLES;
 	}
+
+	int phaseInt=(int)current_phase;
+
+
 	if(interp_cntr < samples_to_interpolate - 1)
 	{
 		interp_cntr++;
--- edisonSynth_linux/src/Oscillator.h
+++ edisonSynth_linux/src/Oscillator.h
@@ -11,10 +11,6 @@
 
 class Oscillator
 {
-	short* sinewaves;
-	double* harm_coeffs1;
-	double* harm_coeffs2;
-	short* harm_coeffs_swap;
 	char coeffs_active; //1 or 2
 	int samples_to_interpolate; // for how many samples the coefficients should be interpolated;
 	int interp_cntr; // the interpolation counter, increases with each get_nextval(), is reset when recalc_coeffs() is called
@@ -22,11 +18,12 @@ class Oscillator
 	double current_frequency;
 	double frequency_1;
 	double frequency_2;
+	double symm1;
+	double symm2;
 	double d_phase;
 	double current_symm; // symm goes from 1 (symmetric) to 0.01 (asymmetric)
 	int waveform; //0: square, 1: triangle/sawtooth
-	double f_cutoff;
-	double resonance; // goes from 0 to -2
+	short*** wavetable;
 
 public:
 	Oscillator();
@@ -36,8 +33,6 @@ public:
 	void reset();
 	void set_symm(double s);
 	void recalc_coeffs(int nsamples,double nfreq);
-	void set_fcutoff(double fc);
-	void set_resonance(double k);
 };
 
 
